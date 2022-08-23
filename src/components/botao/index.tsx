efb99348600e2c2a4cf3747eb38b9fe86004264c
Erikrmb <Erikrmb@gmail.com>
2022-08-22 23:17:27 -0400

    deploy

---
+++ src/components/botao/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from './Botao.module.scss';
+
+interface Props{
+    texto: string
+    type?:"button" | "submit" | "reset" | undefined
+    onClick?: () => void
+}
+
+export default function Botao({texto, type, onClick}: Props){
+    return(
+        <button onClick={onClick} type={type} className={style.botao}>
+            {texto}
+        </button> 
+    )
+}
